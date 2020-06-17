c8e63eef7f236c9d2b7dbd80626f9f8ee15c5f16
Damanotra <redzeroproject@gmail.com>
2020-06-17 20:24:21 +0700

    halfway search tab, preparing firebase

--- lib/ui/screen/home/search.dart
+++ lib/ui/screen/home/search.dart
@@ -1,5 +1,7 @@
+import 'package:datingapps/ui/widget/profilecard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchTab extends StatefulWidget{
   @override
@@ -11,10 +13,28 @@ class _SearchTabState extends State<SearchTab>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation(Colors.indigo),
-      ),
-    );
+    Size size = MediaQuery.of(context).size;
+
+    return Center();
+  }
+
+  Widget userGender(gender) {
+    switch (gender) {
+      case 'Male':
+        return Icon(
+          FontAwesomeIcons.mars,
+          color: Colors.white,
+        );
+        break;
+      case 'Female':
+        return Icon(
+          FontAwesomeIcons.venus,
+          color: Colors.white,
+        );
+        break;
+      default:
+        return null;
+        break;
+    }
   }
 }
